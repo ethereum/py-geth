0d68e5a7c4af5dc0b396668fa958664d7c6d3ce9
Piper Merriam <pipermerriam@gmail.com>
2016-07-26 19:10:09 -0600

    add flaky tests

--- tests/waiting/test_waiting_for_ipc_socket.py
+++ tests/waiting/test_waiting_for_ipc_socket.py
@@ -1,4 +1,5 @@
 import pytest
+from flaky import flaky
 
 import gevent
 
@@ -11,6 +12,7 @@ def test_waiting_for_ipc_socket(base_dir):
         geth.wait_for_ipc(timeout=60)
 
 
+@flaky(max_runs=3)
 def test_timeout_waiting_for_ipc_socket(base_dir):
     with DevGethProcess('testing', base_dir=base_dir) as geth:
         assert geth.is_running
--- tests/waiting/test_waiting_for_rpc_connection.py
+++ tests/waiting/test_waiting_for_rpc_connection.py
@@ -1,4 +1,5 @@
 import pytest
+from flaky import flaky
 
 import gevent
 
@@ -11,7 +12,8 @@ def test_waiting_for_rpc_connection(base_dir):
         assert geth.is_running
 
 
+@flaky(max_runs=3)
 def test_timeout_waiting_for_rpc_connection(base_dir):
     with DevGethProcess('testing', base_dir=base_dir) as geth:
         with pytest.raises(gevent.Timeout):
-            geth.wait_for_rpc(timeout=1)
+            geth.wait_for_rpc(timeout=0.1)
