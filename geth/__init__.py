83940db06e60739a73ab104452cd97dc49b71022
Bhargavasomu <somubhargava97@gmail.com>
2019-02-15 07:50:23 +0530

    Remove support for python2.7

--- geth/__init__.py
+++ geth/__init__.py
@@ -1,6 +1,4 @@
 import pkg_resources
-import sys
-import warnings
 
 from .main import (  # noqa: F401
     get_geth_version,
@@ -19,14 +17,6 @@ from .mixins import (  # noqa: F401
     InterceptedStreamsMixin,
     LoggingMixin,
 )
-
-
-if sys.version_info.major < 3:
-    warnings.simplefilter('always', DeprecationWarning)
-    warnings.warn(DeprecationWarning(
-        "The `py-geth` library is dropping support for Python 2.  Upgrade to Python 3."
-    ))
-    warnings.resetwarnings()
 
 
 __version__ = pkg_resources.get_distribution("py-geth").version
--- geth/mixins.py
+++ geth/mixins.py
@@ -3,13 +3,7 @@ from __future__ import absolute_import
 import datetime
 import logging
 import os
-
-try:
-    import queue
-except ImportError:
-    # python 2 support
-    import Queue as queue
-
+import queue
 import time
 
 from geth.utils.filesystem import ensure_path_exists
