bfbc9e83d500089bb6f38b838c45f8dd4c79a1e9
Jason Carver <ut96caarrs@snkmail.com>
2019-12-04 11:51:16 -0800

    Bump version: 2.1.0 → 2.2.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ deps['dev'] = (
 setup(
     name='py-geth',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='2.1.0',
+    version='2.2.0',
     description="""Run Go-Ethereum as a subprocess""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
