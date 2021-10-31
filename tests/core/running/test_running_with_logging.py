cfa8f74e70c720198dbc21e7d8f50a021e830e87
unparalleled-js <yingthi@live.com>
2021-10-31 11:56:09 -0500

    chore: rm use of global

--- tests/core/running/test_running_with_logging.py
+++ tests/core/running/test_running_with_logging.py
@@ -4,25 +4,22 @@ import threading
 from geth import DevGethProcess
 from geth.mixins import LoggingMixin
 
-_ERRORS = []
+_errors = []
 
 
 @pytest.fixture(autouse=True)
 def fail_from_errors_on_other_threads():
     # Set excepthook to catch errors in child threads.
-    global _ERRORS
-    _ERRORS = []
 
     def pytest_excepthook(*args, **kwargs):
-        global _ERRORS
-        _ERRORS.extend(args)
+        _errors.extend(args)
 
     threading.excepthook = pytest_excepthook
 
     yield
 
-    if _ERRORS:
-        caught_errors_str = ', '.join([str(err) for err in _ERRORS])
+    if _errors:
+        caught_errors_str = ', '.join([str(err) for err in _errors])
         pytest.fail(f'Caught exceptions from other threads:\n{caught_errors_str}')
 
 
