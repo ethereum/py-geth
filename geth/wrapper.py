4310323a7b1410fffeda2897656da9a55d8f0ccc
unparalleled-js <yingthi@live.com>
2021-11-01 16:57:09 -0500

    feat/cache

--- geth/wrapper.py
+++ geth/wrapper.py
@@ -143,7 +143,8 @@ def construct_popen_command(data_dir=None,
                             suffix_args=None,
                             suffix_kwargs=None,
                             shh=None,
-                            allow_insecure_unlock=None):
+                            allow_insecure_unlock=None,
+                            cache=None):
     if geth_executable is None:
         geth_executable = get_geth_binary_path()
 
@@ -243,6 +244,9 @@ def construct_popen_command(data_dir=None,
 
     if allow_insecure_unlock:
         builder.append('--allow-insecure-unlock')
+
+    if cache:
+        builder.extend(("--cache", cache))
 
     if suffix_kwargs:
         builder.extend(suffix_kwargs)
