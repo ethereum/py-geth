4b44af0dda335710df22b50a28ab7c797f3a8a61
Juliya Smith <yingthi@live.com>
2021-11-02 17:43:27 -0500

    fix: handle StopIteration in JoinableQueues (#77)
    * fix: stop iterating on StopIteration in JoinableQueue
    
    * chore: rm unneeded sys hook
    
    * chore: rm use of global
    
    * doc: document fixture purpose
    
    * fix: handle stop iteration

--- tests/core/running/test_running_with_logging.py
+++ tests/core/running/test_running_with_logging.py
@@ -1,5 +1,29 @@
+import pytest
+import threading
+
 from geth import DevGethProcess
 from geth.mixins import LoggingMixin
+
+_errors = []
+
+
+@pytest.fixture(autouse=True)
+def fail_from_errors_on_other_threads():
+    """
+    Causes errors when `LoggingMixin` is improperly implemented.
+    Useful for preventing false-positives in logging-based tests.
+    """
+
+    def pytest_excepthook(*args, **kwargs):
+        _errors.extend(args)
+
+    threading.excepthook = pytest_excepthook
+
+    yield
+
+    if _errors:
+        caught_errors_str = ', '.join([str(err) for err in _errors])
+        pytest.fail(f'Caught exceptions from other threads:\n{caught_errors_str}')
 
 
 class WithLogging(LoggingMixin, DevGethProcess):
