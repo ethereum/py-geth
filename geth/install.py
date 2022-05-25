269ba9998b96b73bef8285c848bcb7a0d07f465a
Felipe Selmo <fselmo2@gmail.com>
2022-05-25 11:18:39 -0600

    Add support for geth v1.10.18

--- geth/install.py
+++ geth/install.py
@@ -40,6 +40,7 @@ V1_10_14 = 'v1.10.14'
 V1_10_15 = 'v1.10.15'
 V1_10_16 = 'v1.10.16'
 V1_10_17 = 'v1.10.17'
+V1_10_18 = 'v1.10.18'
 
 
 LINUX = 'linux'
@@ -332,6 +333,7 @@ install_v1_10_14 = functools.partial(install_from_source_code_release, V1_10_14)
 install_v1_10_15 = functools.partial(install_from_source_code_release, V1_10_15)
 install_v1_10_16 = functools.partial(install_from_source_code_release, V1_10_16)
 install_v1_10_17 = functools.partial(install_from_source_code_release, V1_10_17)
+install_v1_10_18 = functools.partial(install_from_source_code_release, V1_10_18)
 
 
 INSTALL_FUNCTIONS = {
@@ -366,6 +368,7 @@ INSTALL_FUNCTIONS = {
         V1_10_15: install_v1_10_15,
         V1_10_16: install_v1_10_16,
         V1_10_17: install_v1_10_17,
+        V1_10_18: install_v1_10_18,
     },
     OSX: {
         V1_9_14: install_v1_9_14,
@@ -398,6 +401,7 @@ INSTALL_FUNCTIONS = {
         V1_10_15: install_v1_10_15,
         V1_10_16: install_v1_10_16,
         V1_10_17: install_v1_10_17,
+        V1_10_18: install_v1_10_18,
     }
 }
 
