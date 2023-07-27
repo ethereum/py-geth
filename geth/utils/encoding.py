8d2f8534fb61ba6186b58cefc0e464ec41e64e44
pacrob <5199899+pacrob@users.noreply.github.com>
2023-07-27 14:00:53 -0600

    wip

--- geth/utils/encoding.py
+++ geth/utils/encoding.py
@@ -1,32 +1,42 @@
 import codecs
+from typing import (
+    Any,
+    Union,
+)
 
 binary_types = (bytes, bytearray)
 text_types = (str,)
 string_types = (bytes, str, bytearray)
 
 
-def is_binary(value):
+def is_binary(value: Any) -> bool:
     return isinstance(value, binary_types)
 
 
-def is_text(value):
+def is_text(value: Any) -> bool:
     return isinstance(value, text_types)
 
 
-def is_string(value):
+def is_string(value: Any) -> bool:
     return isinstance(value, string_types)
 
 
-def force_bytes(value, encoding="iso-8859-1"):
+def force_bytes(value: Any, encoding: str = "iso-8859-1") -> Union[bytes, bytearray]:
     if is_binary(value):
         return bytes(value)
     elif is_text(value):
-        return codecs.encode(value, encoding)
+        encoded = codecs.encode(value, encoding)
+        if is_binary(encoded):
+            return encoded
+        else:
+            raise TypeError(
+                f"Encoding {encoding!r} produced non-binary result: {encoded!r}"
+            )
     else:
         raise TypeError(f"Unsupported type: {type(value)}")
 
 
-def force_text(value, encoding="iso-8859-1"):
+def force_text(value: Any, encoding: str = "iso-8859-1") -> str:
     if is_text(value):
         return value
     elif is_binary(value):
@@ -35,7 +45,7 @@ def force_text(value, encoding="iso-8859-1"):
         raise TypeError(f"Unsupported type: {type(value)}")
 
 
-def force_obj_to_text(obj):
+def force_obj_to_text(obj: Any) -> Any:
     if is_string(obj):
         return force_text(obj)
     elif isinstance(obj, dict):
