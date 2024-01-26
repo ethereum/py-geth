d2c455bb63e5c82b0b981eda257380b274cde5b1
kclowes <kclowes@users.noreply.github.com>
2024-01-26 13:31:47 -0700

    Bump version: 3.13.0 → 3.14.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ with open("./README.md") as readme:
 setup(
     name="py-geth",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version="3.13.0",
+    version="3.14.0",
     description="""py-geth: Run Go-Ethereum as a subprocess""",
     long_description_content_type="text/markdown",
     long_description=long_description,
