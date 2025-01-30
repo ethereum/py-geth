1393d2c118e56e7aec25ec357e523fd2d582ad95
pacrob <5199899+pacrob@users.noreply.github.com>
2025-01-30 11:05:45 -0700

    add geth v1.14.13 support

--- geth/install.py
+++ geth/install.py
@@ -46,6 +46,7 @@ V1_14_9 = "v1.14.9"
 V1_14_10 = "v1.14.10"
 V1_14_11 = "v1.14.11"
 V1_14_12 = "v1.14.12"
+V1_14_13 = "v1.14.13"
 
 
 LINUX = "linux"
@@ -341,6 +342,7 @@ install_v1_14_9 = functools.partial(install_from_source_code_release, V1_14_9)
 install_v1_14_10 = functools.partial(install_from_source_code_release, V1_14_10)
 install_v1_14_11 = functools.partial(install_from_source_code_release, V1_14_11)
 install_v1_14_12 = functools.partial(install_from_source_code_release, V1_14_12)
+install_v1_14_13 = functools.partial(install_from_source_code_release, V1_14_13)
 
 INSTALL_FUNCTIONS = {
     LINUX: {
@@ -356,6 +358,7 @@ INSTALL_FUNCTIONS = {
         V1_14_10: install_v1_14_10,
         V1_14_11: install_v1_14_11,
         V1_14_12: install_v1_14_12,
+        V1_14_13: install_v1_14_13,
     },
     OSX: {
         V1_14_0: install_v1_14_0,
@@ -370,6 +373,7 @@ INSTALL_FUNCTIONS = {
         V1_14_10: install_v1_14_10,
         V1_14_11: install_v1_14_11,
         V1_14_12: install_v1_14_12,
+        V1_14_13: install_v1_14_13,
     },
 }
 
