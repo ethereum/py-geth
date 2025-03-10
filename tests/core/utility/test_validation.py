7a68fbbd826f353dba2f1bdbb6bd2629101dd92e
fselmo <fselmo2@gmail.com>
2025-03-10 15:02:43 -0600

    Remove type hint validation since no defaults on pydantic models

--- tests/core/utility/test_validation.py
+++ tests/core/utility/test_validation.py
@@ -3,19 +3,11 @@ from __future__ import (
 )
 
 import pytest
-import sys
-from typing import (
-    get_type_hints,
-)
 
 from geth.exceptions import (
     PyGethValueError,
 )
-from geth.types import (
-    GenesisDataTypedDict,
-)
 from geth.utils.validation import (
-    GenesisData,
     validate_genesis_data,
     validate_geth_kwargs,
 )
@@ -98,21 +90,3 @@ def test_validate_genesis_data_good(genesis_data):
 def test_validate_genesis_data_bad(genesis_data):
     with pytest.raises(PyGethValueError):
         validate_genesis_data(genesis_data)
-
-
-@pytest.mark.skipif(sys.version_info < (3, 9), reason="get_type_hints requires >=py39")
-@pytest.mark.parametrize(
-    "model, typed_dict",
-    [
-        (GenesisData, GenesisDataTypedDict),
-    ],
-)
-def test_model_fields_match_typed_dict(model, typed_dict):
-    # Get the fields and types from the Pydantic model
-    model_fields = get_type_hints(model)
-    assert len(model_fields) > 0, "Model has no fields"
-
-    # Get the fields and types from the TypedDict
-    typed_dict_fields = get_type_hints(typed_dict)
-    assert len(typed_dict_fields) > 0, "TypedDict has no fields"
-    assert len(typed_dict_fields) == len(model_fields), "Field counts do not match"
